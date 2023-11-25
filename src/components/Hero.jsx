57515e0cebe7df7142112a96339c67db6c334344
itsabhi-nav <dubeyabhinav100@gmail.com>
2023-11-25 20:50:45 +0530

    second commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,36 +7,27 @@ const Hero = () => {
   return (
     <section className="relative w-full h-screen mx-auto">
       <div
-        className={
-          [styles.paddingX] +
-          " absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5"
-        }
+        className={[
+          styles.paddingX,
+          "absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-col items-center gap-5",
+        ].join(" ")}
       >
         <div className="flex flex-col justify-center items-center mt-5">
           <div className="w-5 h-5 rounded-full bg-[#915eff]" />
           <div className="w-1 sm:h-80 h-40 violet-gradient" />
         </div>
-        <div>
-          <h1
-            className={
-              [styles.heroHeadText] + " text-white text-center sm:text-left"
-            }
-          >
-            Hi, I'm <span className="text-[#B31312]"> Harsh </span>
+        <div className="text-white text-center">
+          <h1 className={[styles.heroHeadText, "mb-2"].join(" ")}>
+            Hi, I'm <span className="text-[#B31312]">Harsh</span>
           </h1>
-          <p
-            className={
-              [styles.heroSubText] +
-              " mt-2 text-white-100 text-center sm:text-left"
-            }
-          >
+          <p className={[styles.heroSubText, "mb-2"].join(" ")}>
             ECE Student | Full-Stack Developer | ML & AI Enthusiast | Blockchain
             Explorer
           </p>
         </div>
       </div>
       <ComputersCanvas />
-      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+      <div className="absolute bottom-10 sm:bottom-32 w-full flex justify-center items-center">
         <a href="#about">
           <div className="w-[32px] h-[48px] rounded-2xl border-4 border-secondary flex justify-center items-start p-1">
             <motion.div
